af03b49c3cfcd9a12b224b6c08514e115fda090d
Le8wa-dev <savinchev@list.ru>
2020-02-21 12:22:49 +0300

    Updates

---
+++ precache-manifest.395c88ae238d2fe18e7a83808ae0a58e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a05b6fabfdc1b0e716481fc1945938d",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "a540294ba00ce5099574",
+    "url": "/social-network/static/css/4.6e3b6046.chunk.css"
+  },
+  {
+    "revision": "de603cc62bf1ac23736b",
+    "url": "/social-network/static/css/5.879bfb13.chunk.css"
+  },
+  {
+    "revision": "a857723dc730054f2041",
+    "url": "/social-network/static/css/6.164cb62f.chunk.css"
+  },
+  {
+    "revision": "a1b78100a6fa425f3ce9",
+    "url": "/social-network/static/css/7.29c65c89.chunk.css"
+  },
+  {
+    "revision": "98e401ddba388294273a",
+    "url": "/social-network/static/css/main.442925a8.chunk.css"
+  },
+  {
+    "revision": "c6ccf538262ce484b919",
+    "url": "/social-network/static/js/0.0e46d99e.chunk.js"
+  },
+  {
+    "revision": "df51b3a7bdd3443b6282",
+    "url": "/social-network/static/js/3.5d5ae918.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/social-network/static/js/3.5d5ae918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a540294ba00ce5099574",
+    "url": "/social-network/static/js/4.17665d6c.chunk.js"
+  },
+  {
+    "revision": "de603cc62bf1ac23736b",
+    "url": "/social-network/static/js/5.b2fb2f1f.chunk.js"
+  },
+  {
+    "revision": "a857723dc730054f2041",
+    "url": "/social-network/static/js/6.9083b0d6.chunk.js"
+  },
+  {
+    "revision": "a1b78100a6fa425f3ce9",
+    "url": "/social-network/static/js/7.72ff61b3.chunk.js"
+  },
+  {
+    "revision": "98e401ddba388294273a",
+    "url": "/social-network/static/js/main.d345bdf3.chunk.js"
+  },
+  {
+    "revision": "56643196b2c1f54a5dd0",
+    "url": "/social-network/static/js/runtime-main.a10a2f9f.js"
+  },
+  {
+    "revision": "e9c664b1f75bad50bd1f2b196b3e081d",
+    "url": "/social-network/static/media/header-logo.e9c664b1.png"
+  },
+  {
+    "revision": "14737803b0a7f00463fd50eed04b0684",
+    "url": "/social-network/static/media/preloader.14737803.svg"
+  }
+]);
